c8543a6b3d480a88a094d2b3544d4b11df430e77
Tom Snee <thomas.snee@quintiles.com>
2020-09-21 15:57:01 -0400

    First few problems of chapter 3.

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -50,13 +50,37 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = ???
+  def tail[A](l: List[A]): List[A] = l match {
+    case Cons(_, t) => t
+    case Nil => Nil
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = ???
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Cons(_, t) => Cons(h, t)
+    case Nil => sys.error("Cannot replace head of empty list.")
+  }
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  def drop[A](l: List[A], n: Int): List[A] = {
+    require(n >= 0, "Numeric argument to drop must be nonnegative.")
+    @annotation.tailrec
+    def loop(list: List[A], counter: Int): List[A] =
+      list match {
+        case Nil => Nil
+        case Cons(_, t) =>
+          if (counter > 0) {
+            loop(t, counter - 1)
+          } else {
+            t
+          }
+      }
+    loop(l, n)
+  }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  @annotation.tailrec
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Nil => Nil
+    case Cons(h, t) => if (f(h)) dropWhile(t, f) else l
+  }
 
   def init[A](l: List[A]): List[A] = ???
 
