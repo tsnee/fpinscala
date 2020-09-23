a4d6b5d939f9b26229c209af751abfbdf1fe6ad6
Tom Snee <351700+tsnee@users.noreply.github.com>
2020-09-22 23:45:28 -0400

    Finished chapter 3.

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -82,11 +82,95 @@ object List { // `List` companion object. Contains functions for creating and wo
     case Cons(h, t) => if (f(h)) dropWhile(t, f) else l
   }
 
-  def init[A](l: List[A]): List[A] = ???
+  def init[A](l: List[A]): List[A] = {
+    def loop(oldHead: A, list: List[A]): List[A] = list match {
+      case Cons(h, t) => Cons(oldHead, loop(h, t))
+      case Nil => Nil
+    }
+    l match {
+      case Cons(h, t) => loop(h, t)
+      case Nil => Nil
+    }
+  }
 
-  def length[A](l: List[A]): Int = ???
+  def length[A](l: List[A]): Int = foldRight(l, 0)((a, b) => b + 1)
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  @annotation.tailrec
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil => z
+    case Cons(h, t) => foldLeft(t, f(z, h))(f)
+  }
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = ???
+  def sum11(ns: List[Int]): Int = foldLeft(ns, 0)(_ + _)
+  def product11(ns: List[Double]): Double = foldLeft(ns, 1.0)(_ * _)
+  def length11[A](ns: List[A]): Int = foldLeft(ns, 0)((b, _) => b + 1)
+
+  def reverse[A](l: List[A]): List[A] = foldLeft(l, Nil: List[A]) { (acc, x) =>
+    Cons(x, acc)
+  }
+
+  def foldRight13[A,B](as: List[A], z: B)(f: (A, B) => B): B = foldLeft(reverse(as), z)((b, a) => f(a, b))
+
+  def foldLeft13[A,B](as: List[A], z: B)(f: (B, A) => B): B = foldRight(reverse(as), z)((a, b) => f(b, a))
+
+  def appendViaFoldRight[A](a1: List[A], a2: List[A]): List[A] = foldRight(a1, a2)(Cons(_, _))
+  def appendViaFoldLeft[A](a1: List[A], a2: List[A]): List[A] = foldLeft(reverse(a1), a2)((b, a) => Cons(a, b))
+
+  def flatten[A](as: List[List[A]]): List[A] = foldRight(as, Nil: List[A])((a, b) => append(a, b))
+
+  def plusOne(xs: List[Int]): List[Int] = xs match {
+    case Cons(h, t) => Cons(h + 1, plusOne(t))
+    case Nil => Nil
+  }
+
+  def doubleToString(xs: List[Double]): List[String] = xs match {
+    case Cons(h, t) => Cons(h.toString, doubleToString(t))
+    case Nil => Nil
+  }
+
+  def doubleToString2(xs: List[Double]): List[String] = foldRight(xs, Nil: List[String])((a, b) => Cons(a.toString, b))
+
+  def map[A,B](l: List[A])(f: A => B): List[B] = l match {
+    case Cons(h, t) => Cons(f(h), map(t)(f))
+    case Nil => Nil
+  }
+
+  def mapAlso[A,B](l: List[A])(f: A => B): List[B] = foldRight(l, Nil: List[B])((a, b) => Cons(f(a), b))
+
+  def filter[A](xs: List[A])(p: A => Boolean): List[A] = xs match {
+    case Cons(h, t) if p(h) => Cons(h, filter(t)(p))
+    case Cons(h, t) => filter(t)(p)
+    case Nil => Nil
+  }
+
+  def flatMap[A,B](as: List[A])(f: A => List[B]): List[B] = flatten(map(as)(f))
+  def flatMap2[A,B](as: List[A])(f: A => List[B]): List[B] = as match {
+    case Cons(h, t) => append(f(h), flatMap2(t)(f))
+    case Nil => Nil
+  }
+
+  def filterViaFlatMap[A](xs: List[A])(p: A => Boolean): List[A] = flatMap(xs)(x => if (p(x)) List(x) else Nil)
+
+  def zipPlus(xs: List[Int], ys: List[Int]): List[Int] = (xs, ys) match {
+    case (Cons(xh, xt), Cons(yh, yt)) => Cons(xh + yh, zipPlus(xt, yt))
+    case _ => Nil
+  }
+
+  def zipWith[A,B,C](xs: List[A], ys: List[B])(f: (A, B) => C): List[C] = (xs, ys) match {
+    case (Cons(xh, xt), Cons(yh, yt)) => Cons(f(xh, yh), zipWith(xt, yt)(f))
+    case _ => Nil
+  }
+
+  def hasSubsequence[A](xs: List[A], ys: List[A]): Boolean = {
+    @annotation.tailrec
+    def loop(sup: List[A], sub: List[A], matching: Boolean): Boolean =
+      (sup, sub) match {
+        case (Cons(xh, xt), Cons(yh, yt)) if xh == yh => loop(xt, yt, true)
+        case (Cons(xh, xt), Cons(yh, yt)) if matching => loop(sup, ys, false)
+        case (Cons(xh, xt), Cons(yh, yt)) => loop(xt, ys, false)
+        case (_, Nil) => matching
+        case (Nil, _) => false
+      }
+    loop(xs, ys, false)
+  }
 }
--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -6,8 +6,36 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
 object Tree {
+  def size(t: Tree[_]): Int = t match {
+    case Branch(left: Tree[_], right: Tree[_]) => 1 + size(left) + size(right)
+    case Leaf(_) => 1
+  }
 
+  def maximum(t: Tree[Int]): Int = t match {
+    case Branch(left: Tree[Int], right: Tree[Int]) => maximum(left) max maximum(right)
+    case Leaf(v) => v
+  }
 
+  def depth(t: Tree[_]): Int = t match {
+    case Branch(left: Tree[_], right: Tree[_]) => 1 + (depth(left) max depth(right))
+    case Leaf(_) => 0
+  }
 
+  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Branch(left: Tree[A], right: Tree[A]) => Branch(map(left)(f), map(right)(f))
+    case Leaf(v) => Leaf(f(v))
+  }
 
+  def fold[A,B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Branch(left: Tree[A], right: Tree[A]) => g(fold(left)(f)(g), fold(right)(f)(g))
+    case Leaf(v) => f(v)
+  }
+
+  def sizeViaFold(t: Tree[_]): Int = fold(t)(_ => 1)(_ + _ + 1)
+
+  def maximumViaFold(t: Tree[Int]): Int = fold(t)(identity)(_ max _)
+
+  def depthViaFold(t: Tree[_]): Int = fold(t)(_ => 0)((x: Int, y: Int) => (x max y) + 1)
+
+  def mapViaFold[A,B](t: Tree[A])(f: A => B): Tree[B] = fold(t)(v => Leaf(f(v)): Tree[B])(Branch(_, _))
 }
